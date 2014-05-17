b8861e57151058987369e8eb425040769570f9ac
Miss Nix <nicolephillips182@gmail.com>
2014-05-17 09:39:47 -0400

    added initial project

---
+++ aviationProject/aviationProject/NHAMenuTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  NHAMenuTableViewController.h
+//  aviationProject
+//
+//  Created by Hackademy on 5/17/14.
+//  Copyright (c) 2014 NHAviation.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DDMenuDrawerViewController;
+
+@interface NHAMenuTableViewController : UITableViewController
+
+@property(nonatomic, weak)DDMenuDrawerViewController* menuDrawerViewController;
+
+@end
---
+++ aviationProject/aviationProject/aviationProject/NHAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NHAAppDelegate.h
+//  aviationProject
+//
+//  Created by Hackademy on 5/17/14.
+//  Copyright (c) 2014 NHAviation.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NHAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
